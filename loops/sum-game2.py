b231cc0b025320d173fabff6c2bd6f608b78908e
toestedbwead <leielcano@gmail.com>
2025-10-15 12:47:56 +0800

    removed the wrong logic oh god i didnt even read that it's sum of odd numbers

--- loops/sum-game2.py
+++ loops/sum-game2.py
@@ -4,11 +4,6 @@ Assignment
 Complete the sum_of_odd_numbers function. It should calculate the sum of all the odd numbers starting at 1 up to (but not including) the given end number and return the result.
 '''
 
-'''def sum_of_odd_numbers(end):
-    total = 1
-    for i in range(0, end):
-        total += i
-    return total'''
 
 def sum_of_odd_numbers(end):
     total = 0
