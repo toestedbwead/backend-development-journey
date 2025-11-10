ff5092064124ef99ab1ee33c974110aad9623bd3
toestedbwead <leielcano@gmail.com>
2025-11-10 22:02:12 +0800

    testing testing

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 # testing just in case im not here later
 
 # another testing
+
+# in a lump rn
+
+def janekao():
+    isJaneKaoDating = True
+
+    if isJaneKaoDating:
+        print("They are real and they are dating.")
+    else:
+        print("Nope, you're just delusional.")
+
+
+
+janekao()
